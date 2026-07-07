ad2712991311708c11382e6f65657c93a987b1af
Lovdesigner <110859597+Lovdesigner@users.noreply.github.com>
2026-07-06 21:31:45 -0300

    Delete auth.routes.js

--- auth.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { login } from "../controllers/auth.controller.js"
-
-const router = express.Router()
-//no olvidar poner exacta la ruta
-router.post("/auth/login", login)
-
-export default router;
